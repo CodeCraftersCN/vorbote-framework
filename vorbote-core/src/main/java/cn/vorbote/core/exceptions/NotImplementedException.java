bd00bf88b27df8955e78541aed6814158c6d4790
theod <theodore.hills@outlook.com>
2022-09-02 16:16:37 +0800

    feat: added NotImplementedException

---
+++ vorbote-core/src/main/java/cn/vorbote/core/exceptions/NotImplementedException.java
@@ -0,0 +1,22 @@
+package cn.vorbote.core.exceptions;
+
+/**
+ * NotImplementedException<br>
+ * Created at 02/09/2022 16:09
+ *
+ * @author theod
+ */
+public class NotImplementedException extends RuntimeException {
+
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public NotImplementedException(String message) {
+        super(message);
+    }
+}
