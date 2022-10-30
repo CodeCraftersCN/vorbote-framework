a5d8a4e4724f2e2570f231126029c41c2a00d775
vorbote <xinke.shao@outlook.com>
2022-10-30 17:23:40 +0800

    fix: fix that jackson cannot serialize MessageResponse

--- vorbote-message-sender-api/src/main/java/cn/vorbote/message/model/MessageResponse.java
+++ vorbote-message-sender-api/src/main/java/cn/vorbote/message/model/MessageResponse.java
@@ -8,16 +8,58 @@ package cn.vorbote.message.model;
  */
 public final class MessageResponse {
 
-    private String message;
-
-    private String code;
+    private final String message;
+    private final String code;
 
     private MessageResponse(String code, String message) {
         this.message = message;
         this.code = code;
     }
 
+    /**
+     * Initialize a message response with a code and a message.
+     *
+     * @param code    the code from the cloud service platform.
+     * @param message the message from the cloud service platform.
+     * @return a response of sending a message.
+     */
     public static MessageResponse initResponse(String code, String message) {
         return new MessageResponse(code, message);
+    }
+
+    /**
+     * Get the message from the server.
+     *
+     * @return the message from the server.
+     */
+    public String message() {
+        return message;
+    }
+
+    /**
+     * Get the code sent from server.
+     *
+     * @return the code from server.
+     */
+    public String code() {
+        return code;
+    }
+
+    /**
+     * Get the message from the server.
+     *
+     * @return the message from the server.
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Get the code sent from server.
+     *
+     * @return the code from server.
+     */
+    public String getCode() {
+        return code;
     }
 }
